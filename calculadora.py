6cea29affade58e41799c5c9e8456c406f42e701
tharlis <tharlis.sampaio@outlook.com>
2022-08-09 19:56:44 -0500

    #3 adcionar os operadores básicos

--- calculadora.py
+++ calculadora.py
@@ -2,3 +2,11 @@ num1 = int(input('Entre com o primeiro numero: '))
 num2 = int(input('Entre com o segundo numero: '))
 
 print(f'{num1}+{num2} =', num1+num2)
+
+# subtração
+print(f'{num1} - {num2} =', num1-num2)
+
+# multiplicação 
+print(f'{num1} * {num2} =', num1 * num2)
+
+print(f'{num1} / {num2} =', num1 + num2)
